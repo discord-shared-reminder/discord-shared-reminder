a1a0161c84b67d80b8bd1cc77234c994c77e9fd6
xnatsuk <xnatsuk@gmail.com>
2023-05-22 20:05:57 -0300

    style: format code

--- packages/service-cdk/lib/components/api-gateway.ts
+++ packages/service-cdk/lib/components/api-gateway.ts
@@ -1,30 +1,29 @@
-import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
-import type * as lambda from "aws-cdk-lib/aws-lambda";
-import type { Construct } from "constructs";
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import type * as lambda from 'aws-cdk-lib/aws-lambda'
+import type { Construct } from 'constructs'
 
 interface ApiGatewayProps {
-    name: string
-    description?: string
+  name: string
+  description?: string
 }
 
 export class ApiGateway {
-    private readonly gateway: RestApi;
+  private readonly gateway: RestApi
 
-    constructor(scope: Construct, id: string, props: ApiGatewayProps) {
-        this.gateway = new RestApi(scope, id, {
-            description: props.description,
-        });
+  constructor(scope: Construct, id: string, props: ApiGatewayProps) {
+    this.gateway = new RestApi(scope, id, {
+      description: props.description,
+    })
 
-        this.gateway.root.addMethod('POST');
-    }
+    this.gateway.root.addMethod('POST')
+  }
 
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    public addProxyLambda(lambdaHandler: lambda.IFunction) {
-        const lambdaIntegration = new LambdaIntegration(lambdaHandler)
+  public addProxyLambda(lambdaHandler: lambda.IFunction) {
+    const lambdaIntegration = new LambdaIntegration(lambdaHandler)
 
-        this.gateway.root.addProxy({
-            anyMethod: true,
-            defaultIntegration: lambdaIntegration
-        })
-    }
+    this.gateway.root.addProxy({
+      anyMethod: true,
+      defaultIntegration: lambdaIntegration,
+    })
+  }
 }
